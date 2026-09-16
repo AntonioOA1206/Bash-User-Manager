ecd9145dcefb0147a91879b564233a7a5b6f594e
AntonioOA1206 <antonio3ocon@gmail.com>
2026-09-16 13:54:59 +0200

    Modificaciones completadas, aun no se ha probado

---
+++ interactive_menu.sh
@@ -0,0 +1,118 @@
+#!/bin/bash
+# Bash script module for a universal interactive menu | AntonioOA1206 & OpenMosto | v2.0
+##
+# This script will display an interactive menu were you can choose between options with W S keys or with the arrows in the terminal.
+# You can import this on your script or program and use the funtion "interactive-menu" followed by a the options you want your menu to have as arguments.
+##
+# Example: interactive_menu "1. Option" "2. Option" "3. Option" "4. Quit"
+## 
+# The selected option will define the variable "menu_option" with its position for the programer to then refer to it (for example, option 1 will define the variable with the number 1)
+# The last option is build to always be the "Quit" option, and will end the program instantly unless you use the -b (or --break) option. 
+##
+# Funtions
+cursor-fix() { # Function to avoid not showing your cursor when you stop the program with CTRL + C
+	tput cnorm
+	exit 0
+}
+##
+function prompt_template ( # Sets the structure for highlighting the options.
+	echo -e "\e[${1}m${@:2}\e[0m"
+)
+##
+function print_menu ( # Prints every iteration of the menu with te selected option highlighted
+	color_increment=1
+	for i in "${!options[@]}"; do
+		option="${options[$i]}"
+		((color_increment++))
+		if  [ $i = $(($menu_position-1)) ]; then
+			prompt_template $1 = = = = = = = =
+			prompt_template ${!color_increment} $option
+			prompt_template $1 = = = = = = = =
+		else
+			prompt_template ${!color_increment} $option
+		fi
+	done
+)
+##
+interactive_menu() {
+	menu_position=1
+	##
+	declare -a options=()
+	for arg in "$@"; do # Build the array of options and search for the break switch
+		if [[ "$arg" != "-b" ]] && [[ "$arg" != "--break" ]]; then
+			options+=("$arg")
+		elif [[ "$arg" == "-b" ]] || [[ "$arg" == "--break" ]]; then
+			quit_option="break"
+		fi
+	done
+	##
+	while true; do # Menu loop
+		tput civis # Hides the terminal cursor
+		##
+		while true; do # Menu print loop
+			# Array that saves the menu arguments, resets every time you move trough the menu. This arguments represent each a color.
+			declare -a arguments=(30) # Argument 30 = equals chain (======)
+			for ((i=1; i<${#options[@]}; i++)); do
+				arguments+=(33) # Argument 33 = Menu option
+			done
+			arguments+=(31) # Argument 31 = "Quit" option (red)
+			arguments[$menu_position]=44 # Argument 44 = Highlighted option
+			##
+			print_menu "${arguments[@]}"
+			trap cursor-fix SIGINT
+			read -s -n 1 key # Read the keyboard imput (WS or up and down arrows)
+			##
+			if [ -z $key ]; then # Enter
+				break
+			elif [ $key = "w" ]; then # W
+				if [ $menu_position -eq 1 ]; then
+					menu_position=1
+				else
+					menu_position=$(($menu_position-1))
+				fi
+			elif [ $key = "s" ]; then # S
+				if [ $menu_position -eq ${#options[@]} ]; then
+					menu_position=${#options[@]}
+				else
+					menu_position=$((menu_position+1))
+				fi
+			elif [[ $key == $'\x1b' ]]; then # Arrow imput (Escape ANSI secuence)
+				read -rsn2 arrow
+				case $arrow in
+					"[A")
+						 # Up arrow
+						if [ $menu_position -eq 1 ]; then
+							menu_position=1
+						else
+							menu_position=$(($menu_position-1))
+						fi
+						;;
+					"[B")
+						 # Down arrow
+						if [ $menu_position -eq ${#options[@]} ]; then
+							menu_position=${#options[@]}
+						else
+							menu_position=$((menu_position+1))
+						fi
+						;;
+				esac
+			else
+				menu_position=$menu_position
+			fi
+			clear
+		done
+		tput cnorm
+		if [ $menu_position -eq ${#options[@]} ]; then # Quit option
+			if [[ "$quit_option" == "break" ]]; then # This will only end the menu loop
+				menu_option=$menu_position
+				break
+			else
+				exit 0 # This will shutdown the script without doing anything else
+			fi
+        else # Any other option	
+            menu_option=$menu_position # This will set the option you chosen. Its value is the position of the option in order.
+            break
+        fi
+		##
+	done
+}
--- scarraybid1.sh
+++ scarraybid1.sh
@@ -2,132 +2,13 @@
 
 clear
 
-#Llama a los dos ficheros que contienen todas las funciones que se necesitan
-source ./fusus.sh
-source ./funciones.sh
+#Llama al fichero del menu interactivo
+source ./interactive_menu.sh
 ##
-
-#Se declara el array con las opciones para el menu principal
-declare -a opcs=("Ver_Usuarios" "Crear_Usuario" "Modificar_Usuario" "Borrar_Usuarios" "Volcar_y_Salir")
-
-#Funcion que muestra el menu principal
-function fmenu (
-	pos=0
-	for e in ${opcs[@]};do
-		if [ $pos -eq 0 ];then
-			if [ $sit -eq 1 ];then
-				fcolores $6 = = = = = = = =
-				fcolores $1 $e
-				fcolores $6 = = = = = = = =
-			else
-				fcolores $1 $e
-			fi
-		elif [ $pos -eq 1 ];then
-			if [ $sit -eq 2 ];then
-				fcolores $6 = = = = = = = =
-				fcolores $2 $e
-				fcolores $6 = = = = = = = =
-			else
-				fcolores $2 $e
-			fi
-		elif [ $pos -eq 2 ];then
-			if [ $sit -eq 3 ];then
-				fcolores $6 = = = = = = = =
-				fcolores $3 $e
-				fcolores $6 = = = = = = = =
-			else
-				fcolores $3 $e
-			fi
-		elif [ $pos -eq 3 ];then
-			if [ $sit -eq 4 ];then
-				fcolores $6 = = = = = = = =
-				fcolores $4 $e
-				fcolores $6 = = = = = = = =
-			else
-				fcolores $4 $e
-			fi
-		else
-			if [ $sit -eq 5 ];then
-				fcolores $6 = = = = = = = =
-				fcolores $5 $e
-				fcolores $6 = = = = = = = =
-			else
-				fcolores $5 $e
-			fi
-		fi
-		pos=$(($pos+1))
-	done
-)
-##
-
-sit=1
-
-#Declaramos el array asociativo para el bidimensional
-declare -A usus
-
-#Lee los usuarios del fichero usuarios.txt
-flusus
-
-#Controlar/Moverte por el menu principal
 while true;do
-	#Oculta el cursor en la terminal
-	tput civis
-	##
+	#Se declara el array con las opciones para el menu principal
+	interactive_menu "1. Ver_Usuarios" "2. Crear_Usuario" "3. Modificar_Usuario" "4. Borrar_Usuarios" "5. Volcar_y_Salir" "6. Salir" --break
 
-	#Moverse por el menu
-	while true;do
-		case $sit in
-			1)
-			fmenu 44 33 33 33 31 30
-			;;
-			2)
-			fmenu 33 44 33 33 31 30
-			;;
-			3)
-			fmenu 33 33 44 33 31 30
-			;;
-			4)
-			fmenu 33 33 33 44 31 30
-			;;
-			5)
-			fmenu 33 33 33 33 44 30
-			;;
-		esac
-		#Lee sin mostrar lo que se escribe y solo una tecla
-		read -s -n 1 tecla
-		#Si le das a enter elige esa opcion
-		if [ -z $tecla ];then
-			op=$sit
-			break
-		##
-		#Si pulsas w subes en el menu poniendo como limite la primera opcion
-		elif [ $tecla = "w" ];then
-			if [ $sit -eq 1 ];then
-				sit=1
-			else
-				sit=$(($sit-1))
-			fi
-		##
-		#Si pulsas s bajas en el menu poniendo como limite la ultima opcion
-		elif [ $tecla = "s" ];then
-			if [ $sit -eq 5 ];then
-				sit=5
-			else
-				sit=$((sit+1))
-			fi
-		##
-		#Si pulsas cualquier otra tecla pues no hace nada
-		else
-			sit=$sit
-		fi
-		##
-		clear
-	done
-	##
-
-	#Muestra de nuevo el cursor en la terminal
-	tput cnorm
-	##
 
 	#Dependiendo de que opcion se haya elegido...
 	case $sit in
@@ -137,7 +18,7 @@ while true;do
 		;;
 		2)
 			#Crear usuarios (en el array)
-	 		fcusus
+			fcusus
 		;;
 		3)
 			#Modificar usuarios (en el array)
@@ -152,34 +33,10 @@ while true;do
 			fvolcar
 			break
 		;;
-		*)
-			#Todo lo demas...
-			fcolores 31 OPCION NO VALIDA
-		;;
 	esac
 	##
-
-	#La opcion 5 es salir
-	if [ $sit -eq 5 ];then
-		break
-	##
-	#Si no pues te pregunta si quieres hacer algo mas o no
-	else
-		read -p "¿Deseas algo mas (S/n)? " emp
-		if [ -z $emp ] || [ $emp = "s" ] || [ $emp = "S" ];then
-			clear
-		else
-			break
-		fi
-	fi
-	##
 done
-##
 
 #Limpia el fichero usuarios.txt pues ya no hacen falta esos usuarios
 : > usuarios.txt
-##
-
-#Despedida con un cowsay de figura aleatoria y multicolor
-fvaca Adios
 ##
